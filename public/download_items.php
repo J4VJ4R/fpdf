956e2aa989066b27d73656b30dfb00c0be7b1a58
xavier <xavier@server1.server1.com>
2023-01-09 23:05:52 -0500

    module download_items created and index modificated

---
+++ public/download_items.php
@@ -0,0 +1,35 @@
+<?php
+//AddPage(orientacion[PORTRAIT, LANDSCAPE], tamaño[A3, A4, A5, LETTER, LEGAL]),
+//SetFont(tipo[COURIER, HELVETICA, ARIAL, TIMES, SYMBOL, ZAPDINGBATS], estilo[normal, B, I, U], tamaño),
+//Cell(ancho, alto, texto, bordes, ?, alineacion, rellnear, link),
+//OutPut(destino[I, D, F, S]), nombre_archivo, utf8)
+//$spdf->Image(ruta, posiciónx, posicióny, alto, ancho, tipo, link)
+//call to file
+require('fpdf/fpdf.php');
+$fpdf = new FPDF();
+//Instantiation
+
+class pdf extends FPDF{
+    public function header(){
+        $this->Image('img/3.png', 0, 0, 216, 280,  'png');
+    }
+    public function footer(){
+        
+    }
+}
+$fpdf = new pdf();
+$fpdf->AddPage('portrait', 'letter');
+$fpdf->SetFont('Arial', 'b', 25);
+//Setting the text color
+$fpdf->SetTextColor(255, 255, 255);
+$fpdf->SetY(110);
+$fpdf->Write(15, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vitae magna in magna condimentum fermentum nec a enim. Nullam a pulvinar turpis. Praesent pulvinar volutpat diam, ut lacinia orci dictum non. Aenean sodales nibh quis mauris mattis pulvinar. Aenean magna ante, lacinia nec sollicitudin at, egestas eu eros.');
+$fpdf->Write(15, '"Juan"');
+// __________________________
+$fpdf->AddPage('portrait', 'letter');
+$fpdf->SetFont('Arial', 'b', 25);
+$fpdf->SetY(110);
+$fpdf->Write(15, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vitae magna in magna condimentum fermentum nec a enim. Nullam a pulvinar turpis. Praesent pulvinar volutpat diam, ut lacinia orci dictum non. Aenean sodales nibh quis mauris mattis pulvinar. Aenean magna ante, lacinia nec sollicitudin at, egestas eu eros.');
+$fpdf->Write(15, '"Juan"');
+$fpdf->Close();
+$fpdf->OutPut('D', 'Mensajes.pdf');
--- public/index.php
+++ public/index.php
@@ -1,33 +1,23 @@
-<?php
-//AddPage(orientacion[PORTRAIT, LANDSCAPE], tamaño[A3, A4, A5, LETTER, LEGAL]),
-//SetFont(tipo[COURIER, HELVETICA, ARIAL, TIMES, SYMBOL, ZAPDINGBATS], estilo[normal, B, I, U], tamaño),
-//Cell(ancho, alto, texto, bordes, ?, alineacion, rellnear, link),
-//OutPut(destino[I, D, F, S]), nombre_archivo, utf8)
-//$spdf->Image(ruta, posiciónx, posicióny, alto, ancho, tipo, link)
-//call to file
-require('fpdf/fpdf.php');
-$fpdf = new FPDF();
-//Instantiation
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GR</title>
+    <link rel="stylesheet" type="text/css" href="/css/styles.css"/>
+</head>
+<body>
+    <div class="container">
 
-class pdf extends FPDF{
-    public function header(){
-        $this->Image('img/3.png', 0, 0, 216, 280,  'png');
-    }
-    public function footer(){
-        
-    }
-}
-$fpdf = new pdf();
-$fpdf->AddPage('portrait', 'letter');
-$fpdf->SetFont('Arial', 'b', 25);
-$fpdf->SetY(80);
-$fpdf->Write(15, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vitae magna in magna condimentum fermentum nec a enim. Nullam a pulvinar turpis. Praesent pulvinar volutpat diam, ut lacinia orci dictum non. Aenean sodales nibh quis mauris mattis pulvinar. Aenean magna ante, lacinia nec sollicitudin at, egestas eu eros.');
-$fpdf->Write(15, '"Juan"');
-// __________________________
-$fpdf->AddPage('portrait', 'letter');
-$fpdf->SetFont('Arial', 'b', 25);
-$fpdf->SetY(80);
-$fpdf->Write(15, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vitae magna in magna condimentum fermentum nec a enim. Nullam a pulvinar turpis. Praesent pulvinar volutpat diam, ut lacinia orci dictum non. Aenean sodales nibh quis mauris mattis pulvinar. Aenean magna ante, lacinia nec sollicitudin at, egestas eu eros.');
-$fpdf->Write(15, '"Juan"');
-$fpdf->Close();
-$fpdf->OutPut('D', 'Mensajes.pdf');
+        <div class="header">
+            <div class="header__button">
+                <a class="header__text" href="download_items.php">
+                    <img src="" alt="">Descargar PDF
+                </a>
+            </div>
+            <div class="header__button--green"></div>
+        </div>
+    </div>
+</body>
+</html>
